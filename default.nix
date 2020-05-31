a3e3f198b806b553373ec285581f11c5b917e96c
Harsh Shandilya <me@msfjarvis.dev>
2020-05-31 19:58:18 +0530

    Initial commit
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

---
+++ default.nix
@@ -0,0 +1,7 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+{
+  lib = import ./lib { inherit pkgs; }; # functions
+  modules = import ./modules; # NixOS modules
+  overlays = import ./overlays; # nixpkgs overlays
+}
