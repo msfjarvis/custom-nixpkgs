99d719063308c9b942069ef33d26d94de632cdb7
Harsh Shandilya <me@msfjarvis.dev>
2020-06-04 22:44:14 +0530

    Revert "overlays: add overlay for rtl8821ce"
    Replaced by msfjarvis.dev/g/dotfiles/1e7e1306bace57267ef50
    
    This reverts commit ed9a4270d2b059b7a7e4a3f4dee877543194da4f.

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,1 @@
-self: super: {
-  rtl8821ce = super.rtl8821ce.override {
-    version = "5.5.2_34066.20200325";
-    src = super.fetchFromGitHub {
-      owner = "tomaspinho";
-      repo = "rtl8821ce";
-      rev = "69765eb288a8dfad3b055b906760b53e02ab1dea";
-      sha256 = "17jiw25k74kv5lnvgycvj2g1n06hbrpjz6p4znk4a62g136rhn4s";
-    };
-  };
-}
+{ }
