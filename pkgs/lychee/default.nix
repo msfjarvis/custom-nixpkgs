da6b3b00f39f9d976f64485acbb18a102b854016
Harsh Shandilya <me@msfjarvis.dev>
2021-01-04 13:06:42 +0530

    pkgs: add lychee
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

---
+++ pkgs/lychee/default.nix
@@ -0,0 +1,27 @@
+with import <nixpkgs> {};
+
+rustPlatform.buildRustPackage rec {
+  pname = "lychee";
+  version = "0.4.1";
+  # Tests use network and are impure in nature
+  doCheck = false;
+
+  src = fetchFromGitHub {
+    owner = "lycheeverse";
+    repo = "lychee";
+    rev = "v${version}";
+    sha256 = "08accjk7lsab8qg2f8nmi8f6h7gncs4b090yv2sca456f6yavnhq";
+  };
+
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ openssl ];
+
+  cargoSha256 = "01m5zvcrc3yc8kwnbp59lgc9nj06pxsxx6i3gaqmh9ah587m7rm6";
+
+  meta = with stdenv.lib; {
+    description = "A glorious link checker";
+    homepage = "https://github.com/lycheeverse/lychee";
+    license = licenses.mit;
+    platforms = platforms.unix;
+  };
+}
