957d24bf32ceae7c908fa173ee5ecbf170e272ca
Harsh Shandilya <me@msfjarvis.dev>
2021-04-14 00:43:23 +0530

    pkgs/natls: fix cargo hash

--- pkgs/natls/default.nix
+++ pkgs/natls/default.nix
@@ -11,7 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0629nfcf43pzkx1rp6k1nr5sdpsjsgrs21f8yc7g20kxlnppc7z3";
   };
 
-  cargoSha256 = "1qkcg9rysiqcjzb6x23kgbpvxn91r19qjv2vx2s79y91j5pyn0ms";
+  cargoSha256 = "022wad6d2qm515m7v8qx1cgilxygg63mlqlxfaql5cm1z6chfvh2";
 
   meta = with pkgs.lib; {
     description = "the 'ls' replacement you never knew you needed";
