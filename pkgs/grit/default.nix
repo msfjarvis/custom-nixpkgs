3cc8a1e81d2fee94ce5ecb37daf79beed4508144
Harsh Shandilya <me@msfjarvis.dev>
2021-04-15 22:49:01 +0530

    pkgs/grit: bump to v0.3.0
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- pkgs/grit/default.nix
+++ pkgs/grit/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "grit";
-  version = "0.2.0";
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "climech";
     repo = "grit";
     rev = "v${version}";
-    sha256 = "0v4i8xdf1pgkmwad5jb6n6s4rx48zk57wij0ppzg6zb725wy7r8a";
+    sha256 = "0sqfinl192bq81iqj7g93ip7ii64jcwyfjscx4nrp3n5cp103k3k";
   };
 
   vendorSha256 = "0a1lqfn710fgvrvbimd92102fhjs1wa7r8i0l7s5m7jxks629hw8";
