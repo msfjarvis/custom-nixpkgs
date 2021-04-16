36228b05e5b2f8aa4e6a9cdd99c8444e36df9f3d
Harsh Shandilya <me@msfjarvis.dev>
2021-04-16 23:55:06 +0530

    xtask: add cargo backed runner
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

---
+++ xtask/src/main.rs
@@ -0,0 +1,37 @@
+use std::error::Error;
+use std::process::Command;
+use walkdir::WalkDir;
+
+fn main() -> Result<(), Box<dyn Error + 'static>> {
+    let args = std::env::args();
+    if args.len() == 0 {
+        println!("Provide a command");
+        return Ok(());
+    }
+    match std::env::args().nth(1) {
+        None => {}
+        Some(arg) => match arg.as_str() {
+            "clean" => {
+                for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+                    if entry.path().to_str().unwrap().contains("./result") {
+                        std::fs::remove_file(entry.path().to_str().unwrap())?;
+                    }
+                }
+            }
+            "hash" => {
+                let repo = std::env::args().nth(2).unwrap();
+                let tag = std::env::args().nth(3).unwrap();
+                let url = format!(
+                    "https://github.com/{}/archive/{}.tar.gz",
+                    repo, tag
+                );
+                let output = Command::new("nix-prefetch-url")
+                    .args(&["--type", "sha256", "--unpack", url.as_str()])
+                    .output()?;
+                println!("{}", std::str::from_utf8(&output.stdout).unwrap());
+            }
+            _ => {}
+        },
+    };
+    return Ok(());
+}
