8c8f1688d18395acd1b515e9daa94ec37292cdbe
Harsh Shandilya <me@msfjarvis.dev>
2021-04-17 17:34:23 +0530

    pkgs/lychee: bump to 0.7.0
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- pkgs/lychee/default.nix
+++ pkgs/lychee/default.nix
@@ -2,22 +2,22 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "lychee";
-  version = "0.6.0";
+  version = "0.7.0";
   # Tests use network and are impure in nature
   doCheck = false;
 
   src = fetchFromGitHub {
     owner = "lycheeverse";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "05wrmy1p9ycn57jsj7p3x1w9cbf310s9cck49zxdmkahksbs13aq";
+    rev = version;
+    sha256 = "0kpwpbv0dqb0p4bxjlcjas6x1n91rdsvy2psrc1nyr1sh6gb1q5j";
   };
 
   nativeBuildInputs = [ pkgs.pkgconfig ];
   buildInputs = [ pkgs.openssl ] ++ pkgs.lib.optionals stdenv.isDarwin
     [ pkgs.darwin.apple_sdk.frameworks.Security pkgs.libiconvReal ];
 
-  cargoSha256 = "072k4wnsxpvakb56wwzx54zc1irii8pkg9bvsy76w4xi6kydrjw8";
+  cargoSha256 = "1b915zkg41n3azk4hhg6fgc83n7iq8p7drvdyil2m2a4qdjvp9r3";
 
   meta = with pkgs.lib; {
     description = "A glorious link checker";
