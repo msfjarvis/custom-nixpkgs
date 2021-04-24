ce51588e285e29593bd6fc0585aad3a84a6253af
Harsh Shandilya <me@msfjarvis.dev>
2021-04-24 21:04:17 +0530

    xtask: refactor to use clap and color_eyre
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- xtask/src/cmds.rs
+++ xtask/src/cmds.rs
@@ -1,9 +1,9 @@
+use color_eyre::eyre::Result;
 use std::process::Command;
-use std::{env::Args, error::Error};
 
 use walkdir::WalkDir;
 
-pub(crate) fn clean_derivations() -> Result<(), Box<dyn Error>> {
+pub(crate) fn clean_derivations() -> Result<()> {
     for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
         if entry.path().to_str().unwrap().contains("./result") {
             std::fs::remove_file(entry.path().to_str().unwrap())?;
@@ -12,22 +12,8 @@ pub(crate) fn clean_derivations() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub(crate) fn get_prefetch_hash(mut args: Args) -> Result<(), Box<dyn Error>> {
-    let repo = args.nth(2);
-    let tag = args.nth(3);
-    if repo.is_none() {
-        println!("Providing a repository is necessary");
-        return Ok(());
-    }
-    if tag.is_none() {
-        println!("Providing a tag is necessary");
-        return Ok(());
-    }
-    let url = format!(
-        "https://github.com/{}/archive/{}.tar.gz",
-        repo.unwrap(),
-        tag.unwrap(),
-    );
+pub(crate) fn get_prefetch_hash(repo: String, tag: String) -> Result<()> {
+    let url = format!("https://github.com/{}/archive/{}.tar.gz", repo, tag,);
     let output = Command::new("nix-prefetch-url")
         .args(&["--type", "sha256", "--unpack", url.as_str()])
         .output()?;
@@ -35,7 +21,7 @@ pub(crate) fn get_prefetch_hash(mut args: Args) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub(crate) fn run_nixpkgs_fmt() -> Result<(), Box<dyn Error>> {
+pub(crate) fn run_nixpkgs_fmt() -> Result<()> {
     let mut cmd = Command::new("nixpkgs-fmt");
     for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
         if entry.path().to_str().unwrap().contains(".nix") {
@@ -44,17 +30,5 @@ pub(crate) fn run_nixpkgs_fmt() -> Result<(), Box<dyn Error>> {
     }
     let output = cmd.output()?;
     println!("{}", std::str::from_utf8(&output.stderr).unwrap());
-    Ok(())
-}
-
-pub(crate) fn help() -> Result<(), Box<dyn Error>> {
-    println!(
-        r#"
-Available commands:
-- clean
-- format/fmt
-- hash <owner/repo> <tag>
-"#
-    );
     Ok(())
 }
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,15 +1,72 @@
 mod cmds;
+use clap::{crate_authors, crate_description, crate_name, crate_version, AppSettings, Clap};
 
-use std::error::Error;
+use color_eyre::eyre::{eyre, Result};
+#[derive(Clap)]
+#[clap(
+    name = crate_name!(),
+    version = crate_version!(),
+    author = crate_authors!(),
+    about = crate_description!(),
+    setting = AppSettings::ColoredHelp,
+    setting = AppSettings::DeriveDisplayOrder,
+    setting = AppSettings::SubcommandRequiredElseHelp,
+)]
+struct Opts {
+    #[clap(subcommand)]
+    subcommand: SubCommand,
+}
 
-fn main() -> Result<(), Box<dyn Error + 'static>> {
-    match std::env::args().nth(1) {
-        None => Ok(()),
-        Some(arg) => match arg.as_str() {
-            "clean" => cmds::clean_derivations(),
-            "hash" => cmds::get_prefetch_hash(std::env::args()),
-            "format" | "fmt" => cmds::run_nixpkgs_fmt(),
-            _ => cmds::help(),
-        },
+#[derive(Clap)]
+enum SubCommand {
+    Clean(Clean),
+    Format(Format),
+    Hash(Hash),
+}
+
+/// Delete leftover directories from nix-build tests
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Clean {}
+
+/// Run nixpkgs-format on the entire directory
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Format {}
+
+/// Use nix-prefetch-url to compute the hash for a given GitHub repository at the given revision
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Hash {
+    /// Repository name, of the format OWNER/REPO
+    #[clap(validator = is_valid_repo_format)]
+    repository: String,
+    /// Revision to compute hash at
+    revision: String,
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let opts = Opts::parse();
+    match opts.subcommand {
+        SubCommand::Clean(_) => cmds::clean_derivations(),
+        SubCommand::Format(_) => cmds::run_nixpkgs_fmt(),
+        SubCommand::Hash(hash) => cmds::get_prefetch_hash(hash.repository, hash.revision),
+    }
+}
+
+fn is_valid_repo_format(value: &str) -> Result<()> {
+    if value.contains("/") {
+        let split = value.split("/");
+        if split.clone().count() == 2 {
+            for s in split {
+                if s.is_empty() {
+                    return Err(eyre!("Neither of OWNER or REPO must be empty"));
+                }
+            }
+        }
+        Ok(())
+    } else {
+        Err(eyre!("Value must of the format OWNER/REPO"))
     }
 }
