1e988e358e6cf8ed8c71a01024f4ec49d96cc67d
Harsh Shandilya <me@msfjarvis.dev>
2021-07-21 03:46:24 +0530

    Add update workflow

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+declare -a ITEMS=(
+	adx
+	fclones
+	git-branchless
+	git-quickfix
+	grit
+	helix
+	lychee
+	natls
+	rust-script
+)
+
+for item in "${ITEMS[@]}"; do
+  nix-update --commit --build "${item}"
+done
