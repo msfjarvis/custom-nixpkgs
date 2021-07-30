57b04ff83c0d09bd320f2dff192bf02be2739b52
Harsh Shandilya <me@msfjarvis.dev>
2021-07-30 21:59:25 +0530

    jetbrains-mono-nerdfonts: 2.1.0 -> 2.4.2

--- pkgs/jetbrains-mono-nerdfonts/default.nix
+++ pkgs/jetbrains-mono-nerdfonts/default.nix
@@ -1,13 +1,13 @@
 { lib, fetchzip }:
 
-let version = "2.1.0";
+let version = "2.4.2";
 in fetchzip rec {
   name = "JetBrainsMono_NerdFonts-${version}";
 
   url =
-    "https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/JetBrainsMono.zip";
+    "https://github.com/msfjarvis/random-bits-and-pieces/releases/download/JetBrainsMono-2.242-NerdFonts/JetBrainsMono-2.242-NerdFonts.zip";
 
-  sha256 = "0pikmfs9wlmclzk3l8p57jknzv9vgdsyhfzdxsdy709l6yqhavkw";
+  sha256 = "sha256-LbxpswJ4ju793Ojwj3g5Dwf7NbDSEDtY2eUBhPSxJvo=";
 
   postFetch = ''
     mkdir -p $out/share/fonts
