4444908ca8851aba760ede8951a692362db52817
Harsh Shandilya <me@msfjarvis.dev>
2021-11-28 16:14:53 +0530

    lib: update to latest Rust nightly

--- lib/mk-rust-platform.nix
+++ lib/mk-rust-platform.nix
@@ -4,10 +4,10 @@ let
   mozillaOverlay = pkgs.fetchFromGitHub {
     owner = "mozilla";
     repo = "nixpkgs-mozilla";
-    rev = "0510159186dd2ef46e5464484fbdf119393afa58";
-    sha256 = "sha256-HJX4Pc5ZUAg4apxB/XHuJ+6ukzvRQqeZMjscOBst2bA=";
+    rev = "cbc7435f5b0b3d17b16fb1d20cf7b616eec5e093";
+    sha256 = "sha256-LKqAcdL+woWeYajs02bDQ7q8rsqgXuzhC354NoRaV80=";
   };
-  date = "2021-11-13";
+  date = "2021-11-28";
   channel = "nightly";
   mozilla = pkgs.callPackage "${mozillaOverlay.out}/package-set.nix" { };
   rustSpecific = (mozilla.rustChannelOf { inherit date channel; }).rust;
