e2a001c8688d4bfe1af995ccb36af9a6c816ab21
Harsh Shandilya <me@msfjarvis.dev>
2021-12-23 18:52:47 +0530

    lib: bump Rust nightly

--- lib/mk-rust-platform.nix
+++ lib/mk-rust-platform.nix
@@ -4,10 +4,10 @@ let
   mozillaOverlay = pkgs.fetchFromGitHub {
     owner = "mozilla";
     repo = "nixpkgs-mozilla";
-    rev = "cbc7435f5b0b3d17b16fb1d20cf7b616eec5e093";
-    sha256 = "sha256-LKqAcdL+woWeYajs02bDQ7q8rsqgXuzhC354NoRaV80=";
+    rev = "7c1e8b1dd6ed0043fb4ee0b12b815256b0b9de6f";
+    sha256 = "sha256-FMYV6rVtvSIfthgC1sK1xugh3y7muoQcvduMdriz4ag=";
   };
-  date = "2021-11-28";
+  date = "2021-12-19";
   channel = "nightly";
   mozilla = pkgs.callPackage "${mozillaOverlay.out}/package-set.nix" { };
   rustSpecific = (mozilla.rustChannelOf { inherit date channel; }).rust;
