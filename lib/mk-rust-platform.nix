0736f23f34d216a039453959932e69d549c7e73c
Harsh Shandilya <me@msfjarvis.dev>
2022-02-17 13:28:47 +0530

    lib: bump rust nightly

--- lib/mk-rust-platform.nix
+++ lib/mk-rust-platform.nix
@@ -7,7 +7,7 @@ let
     rev = "7c1e8b1dd6ed0043fb4ee0b12b815256b0b9de6f";
     sha256 = "sha256-FMYV6rVtvSIfthgC1sK1xugh3y7muoQcvduMdriz4ag=";
   };
-  date = "2022-01-01";
+  date = "2022-02-14";
   channel = "nightly";
   mozilla = pkgs.callPackage "${mozillaOverlay.out}/package-set.nix" { };
   rustSpecific = (mozilla.rustChannelOf { inherit date channel; }).rust;
