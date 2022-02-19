be8f495ea3e00c6e3b0a83808a31a3f7ced5f108
Harsh Shandilya <me@msfjarvis.dev>
2022-02-20 03:16:53 +0530

    clipboard-substitutor: 0.4.2 -> 0.4.3

--- pkgs/clipboard-substitutor/default.nix
+++ pkgs/clipboard-substitutor/default.nix
@@ -3,16 +3,16 @@
 
 mkRustPlatform.buildRustPackage rec {
   pname = "clipboard-substitutor";
-  version = "0.4.2";
+  version = "0.4.3";
 
   src = fetchFromGitHub {
     owner = "msfjarvis";
     repo = "clipboard-substitutor";
     rev = "v${version}";
-    sha256 = "sha256-dZ/1JfPZ5skofl8MYK0ExVMhlz5gqtpErK254DO7fE8=";
+    sha256 = "sha256-TLqtZL5XY0+aOCJJHRijs0WPE6HQbjk2OxbFBewQmCE=";
   };
 
-  cargoSha256 = "sha256-b1R6vrcVSOTKlbSvjKPtR07ajCY6Voh2GfFpEn3JAZg=";
+  cargoSha256 = "sha256-4bJQ2KwnI7mqDc/P60XGfJnL+n8TK4hS5EN2PGAaPqA=";
 
   nativeBuildInputs = [ pkg-config pkgs.python3 ];
   buildInputs = pkgs.lib.optionals stdenv.isLinux [ pkgs.xorg.libxcb ]
