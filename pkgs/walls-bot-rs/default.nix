ccf739bbb2f6d307848e28b3794e6b16bae2a1bc
Harsh Shandilya <me@msfjarvis.dev>
2022-11-07 12:09:31 +0530

    pkgs/walls-bot-rs: rewrite to use nightly Rust
    The crate uses `let_chains` which is not stable yet Sadge

--- pkgs/walls-bot-rs/default.nix
+++ pkgs/walls-bot-rs/default.nix
@@ -1,6 +1,23 @@
 { stdenv, fetchFromGitHub, rustPlatform, pkgs ? import <nixpkgs> { } }:
 
-rustPlatform.buildRustPackage rec {
+let
+  rustOverlay = import (builtins.fetchTarball {
+    url =
+      "https://github.com/oxalica/rust-overlay/archive/528499f67169671e931cf1ba63601803e58abd2a.tar.gz";
+    sha256 = "sha256-Yid92tEY2CeS8VUiexjSw0hsjfQbRLbUCpqkdVLaEnI=";
+  });
+  rustPkgs = import <nixpkgs> { overlays = [ rustOverlay ]; };
+  rustSpecific = rustPkgs.rust-bin.nightly."2022-10-14".default.override {
+    extensions = [ ];
+    targets = pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
+      ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ "x86_64-unknown-linux-gnu" ];
+  };
+  nightlyRustPlatform = pkgs.makeRustPlatform {
+    cargo = rustSpecific;
+    rustc = rustSpecific;
+  };
+
+in nightlyRustPlatform.buildRustPackage rec {
   pname = "walls-bot-rs";
   version = "0.19.2";
 
