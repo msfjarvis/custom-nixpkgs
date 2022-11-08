4047d3eaf2dbf36dfb419ffe1a8e041cebd4d3a1
Harsh Shandilya <me@msfjarvis.dev>
2022-11-08 13:01:48 +0530

    gdrive: 3.0.11 -> 3.0.12

--- pkgs/gdrive/default.nix
+++ pkgs/gdrive/default.nix
@@ -2,16 +2,16 @@
 
 buildGo118Module rec {
   pname = "gdrive";
-  version = "3.0.11";
+  version = "3.0.12";
 
   src = fetchFromGitHub {
     owner = "msfjarvis";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-NVAa33lyqtvqpVT5/MzyI8k6bJVfnqxhjKf+BwpXCWU=";
+    sha256 = "sha256-50UchzfAsALt4D9L1EtUWiFUfY7OYQ/Uv0KoeDs0xzo=";
   };
 
-  vendorSha256 = "sha256-2ozu7fGuGFGypddtU+2I39yBA8swSFnGgBd0ScCWucE=";
+  vendorSha256 = "sha256-TX0uGtyiygmOudY8zfgKqYAjFOWYiW3XOw9GaOMaW6o=";
 
   meta = with lib; {
     description = "GDrive CLI tool";
