d8b7c2ecfbb89c9fa873f45de050d1dcb3cd6cc0
Harsh Shandilya <me@msfjarvis.dev>
2022-11-23 17:14:41 +0530

    feat: setup Nix flakes

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "My personal NUR repository";
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+  outputs = { self, nixpkgs, }@inputs:
+    let
+      systems = [ "x86_64-linux" "x86_64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      packages = forAllSystems (system:
+        import ./default.nix { pkgs = import nixpkgs { inherit system; }; });
+    };
+}
