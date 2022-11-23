1cc2c8a1da7533e0e50fd3fbf3679747dd73454b
Harsh Shandilya <me@msfjarvis.dev>
2022-11-23 17:28:49 +0530

    fix(walls-bot-rs): support pure evaluation mode

--- pkgs/walls-bot-rs/default.nix
+++ pkgs/walls-bot-rs/default.nix
@@ -1,18 +1,21 @@
 { stdenv, fetchFromGitHub, rustPlatform, pkgs ? import <nixpkgs> { } }:
 
 let
-  rustOverlay = import (builtins.fetchTarball {
+  rust-overlay = import (builtins.fetchTarball {
     url =
       "https://github.com/oxalica/rust-overlay/archive/528499f67169671e931cf1ba63601803e58abd2a.tar.gz";
     sha256 = "sha256-Yid92tEY2CeS8VUiexjSw0hsjfQbRLbUCpqkdVLaEnI=";
   });
-  rustPkgs = import <nixpkgs> { overlays = [ rustOverlay ]; };
+  rustPkgs = if pkgs.lib.hasAttrByPath [ "rust-bin" ] pkgs then
+    pkgs
+  else
+    pkgs.lib.fix (pkgs.lib.extends rust-overlay (self: pkgs));
   rustSpecific = rustPkgs.rust-bin.nightly."2022-10-14".default.override {
     extensions = [ ];
     targets = pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
       ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ "x86_64-unknown-linux-gnu" ];
   };
-  nightlyRustPlatform = pkgs.makeRustPlatform {
+  nightlyRustPlatform = rustPkgs.makeRustPlatform {
     cargo = rustSpecific;
     rustc = rustSpecific;
   };
