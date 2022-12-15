b46d2d236cc7c6e0896894d0ac87cb31a5706104
Harsh Shandilya <me@msfjarvis.dev>
2022-12-15 11:24:55 +0000

    feat(nix): add aarch64-linux system

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
   outputs = { self, nixpkgs, }@inputs:
     let
-      systems = [ "x86_64-linux" "x86_64-darwin" ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in {
       packages = forAllSystems (system:
