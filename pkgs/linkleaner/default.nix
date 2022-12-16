1214c3bfcd63d56126e39d5745b8317192fec161
Harsh Shandilya <me@msfjarvis.dev>
2022-12-16 15:05:01 +0000

    linkleaner: 1.0.0 -> 1.1.0
    Diff: https://github.com/msfjarvis/linkleaner/compare/v1.0.0...v1.1.0

--- pkgs/linkleaner/default.nix
+++ pkgs/linkleaner/default.nix
@@ -3,14 +3,14 @@
 let
   rust-overlay = import (builtins.fetchTarball {
     url =
-      "https://github.com/oxalica/rust-overlay/archive/5200195aa2a0cef1becc2ba6ff61cba65e1f90fe.tar.gz";
-    sha256 = "sha256-3oGeJgeA4+8Wj2XZnbZKJYHoZziyJl36ZGU49I2VC8U=";
+      "https://github.com/oxalica/rust-overlay/archive/7da2f6b3a0c32f661cb2864d7fbd1d7e6f0c7543.tar.gz";
+    sha256 = "sha256-gvQaOKaV1UK6IzsFzkVLsEavGxnAsQFT3zUqcg0RXLU=";
   });
   rustPkgs = if pkgs.lib.hasAttrByPath [ "rust-bin" ] pkgs then
     pkgs
   else
     pkgs.lib.fix (pkgs.lib.extends rust-overlay (self: pkgs));
-  rustSpecific = rustPkgs.rust-bin.nightly."2022-11-14".default.override {
+  rustSpecific = rustPkgs.rust-bin.nightly."2022-12-15".default.override {
     extensions = [ ];
     targets = pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
       ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ "x86_64-unknown-linux-gnu" ];
@@ -22,19 +22,19 @@ let
 
 in nightlyRustPlatform.buildRustPackage rec {
   pname = "linkleaner";
-  version = "1.0.0";
+  version = "1.1.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "msfjarvis";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-2Ne9L7LOBK0flxAxiq2hSr4XEozL360kKa2EbS/FslM=";
+    sha256 = "sha256-feXKW9ytssgT+IEmRe86WiuH9j2aBVOPVqwFXrA4BMU=";
   };
 
   buildInputs = pkgs.lib.optionals stdenv.isDarwin
     [ pkgs.darwin.apple_sdk.frameworks.Security ];
 
-  cargoSha256 = "sha256-MopVmYjuYi5/fK4+/x1+40sBP87Fw2AE918x/OLcoD4=";
+  cargoSha256 = "sha256-Iuazco7vAVNwy/hfzFBNL7eiN9sTXXNx176NaPngNQE=";
 
   meta = with pkgs.lib; {
     description = "A Telegram bot with an identity crisis";
