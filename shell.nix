03f898fc4bedacd7386c6bbde2c74a0efe65d2a9
Harsh Shandilya <me@msfjarvis.dev>
2022-12-18 21:10:44 +0530

    refactor: switch to alejandra as the Nix formatter

--- shell.nix
+++ shell.nix
@@ -1,3 +1,3 @@
 { pkgs ? import <nixpkgs> { } }:
 
-pkgs.mkShell { buildInputs = with pkgs; [ git micro nixfmt nix-update shfmt ]; }
+pkgs.mkShell { buildInputs = with pkgs; [ alejandra git micro nix-update shfmt ]; }
