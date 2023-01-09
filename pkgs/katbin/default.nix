7b56bfaa38cc1a06846e7ca96c74698fcc0d9d0c
Harsh Shandilya <me@msfjarvis.dev>
2023-01-09 22:16:17 +0530

    katbin: 1.3.13 -> 1.3.14-develop.1
    Diff: https://github.com/SphericalKat/katbin-cli/compare/v1.3.13...v1.3.14-develop.1

--- pkgs/katbin/default.nix
+++ pkgs/katbin/default.nix
@@ -4,20 +4,20 @@
 }:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "katbin";
-  version = "1.3.13";
+  version = "1.3.14-develop.1";
 
   src = pkgs.fetchFromGitHub {
     owner = "SphericalKat";
     repo = "katbin-cli";
     rev = "v${version}";
-    sha256 = "sha256-thL2NUaE6UNPiEjsmNHcbcF7/Rs2JZcPDQq5sL0roYU=";
+    sha256 = "sha256-0DiXswIUyLRIpVM26x6Q/efEG1ueu8CgaUoto7KBfmA=";
   };
 
   buildInputs =
     pkgs.lib.optionals stdenv.isDarwin
     [pkgs.darwin.apple_sdk.frameworks.Security];
 
-  cargoSha256 = "sha256-qfeHd6ry1NhKFH3UnQggJRF4nWnkr47kr4bL5W9Yo3E=";
+  cargoSha256 = "sha256-zI3YZ8m9YQ1+ZWiRyX4nPAP86BKpSg80G+dabkePGhk=";
 
   meta = with pkgs.lib; {
     description = "A CLI for katbin";
