195d2dcfb377e770419a9a1b17c134d347f9b186
Harsh Shandilya <me@msfjarvis.dev>
2023-01-31 20:42:20 +0000

    feat: support `aarch64-darwin`

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     self,
     nixpkgs,
   } @ inputs: let
-    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux"];
+    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     packages =
