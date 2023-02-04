f29a2c4f1f2e14f03edabed8d57123d17db54c47
Harsh Shandilya <me@msfjarvis.dev>
2023-02-04 21:40:48 +0000

    file-collector: 0.2.7 -> 0.2.8
    Diff: https://github.com/msfjarvis/file-collector/compare/v0.2.7...v0.2.8

--- pkgs/file-collector/default.nix
+++ pkgs/file-collector/default.nix
@@ -4,17 +4,17 @@
 }:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "file-collector";
-  version = "0.2.7";
+  version = "0.2.8";
 
   src = pkgs.fetchFromGitHub {
     owner = "msfjarvis";
     repo = "file-collector";
     rev = "v${version}";
-    sha256 = "sha256-mbYRFSG0Cxn3ruO4I1VdduoS+KnCmU7WfGir2fWkfSI=";
+    sha256 = "sha256-hPg9+hZw0Xvlclq/+om8ApkSgWN2wdQb8GEL0QojgXA=";
   };
 
   buildFeatures = pkgs.lib.optionals stdenv.isLinux ["journald"];
-  cargoHash = "sha256-3QTD9tx6oe17wNQ2iWFtqAueAa48NGJWkVi/NaYDeco=";
+  cargoHash = "sha256-41gHv9tqo6TCfiUWKdKCpm5dZjXistbjODWdWI3f+Nw=";
 
   meta = with pkgs.lib; {
     description = "Simple CLI tool to watch directories and move their files to a single dumping ground";
