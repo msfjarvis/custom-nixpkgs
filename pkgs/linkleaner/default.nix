a9da867d3a1a4256e79dbc40d52953973e346d57
Harsh Shandilya <me@msfjarvis.dev>
2023-03-22 11:28:34 +0530

    pkgs: make linkleaner use Rust toolchain from source repo

--- pkgs/linkleaner/default.nix
+++ pkgs/linkleaner/default.nix
@@ -2,35 +2,31 @@
   stdenv,
   pkgs,
 }: let
+  pname = "linkleaner";
+  version = "1.5.0";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "msfjarvis";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-Yhyi7XsC/j2yPxXcEF/UROVzsjeJ5mRk+K4evRrlTfI=";
+  };
   rust-overlay = import (builtins.fetchTarball {
-    url = "https://github.com/oxalica/rust-overlay/archive/7da2f6b3a0c32f661cb2864d7fbd1d7e6f0c7543.tar.gz";
-    sha256 = "sha256-gvQaOKaV1UK6IzsFzkVLsEavGxnAsQFT3zUqcg0RXLU=";
+    url = "https://github.com/oxalica/rust-overlay/archive/32e4b1bf80cf99b7c72d2892198d40558828e6a1.tar.gz";
+    sha256 = "sha256-OZxPSyEDJTy4IATwFxUGDEEUYL6ZkgKn2FhlRlcLE08=";
   });
   rustPkgs =
     if pkgs.lib.hasAttrByPath ["rust-bin"] pkgs
     then pkgs
     else pkgs.lib.fix (pkgs.lib.extends rust-overlay (self: pkgs));
-  rustSpecific = rustPkgs.rust-bin.nightly."2022-12-15".default.override {
-    extensions = [];
-    targets =
-      pkgs.lib.optionals pkgs.stdenv.isDarwin ["aarch64-apple-darwin"]
-      ++ pkgs.lib.optionals pkgs.stdenv.isLinux ["x86_64-unknown-linux-gnu"];
-  };
+  rustSpecific = rustPkgs.rust-bin.fromRustupToolchainFile "${src}/rust-toolchain.toml";
   nightlyRustPlatform = rustPkgs.makeRustPlatform {
     cargo = rustSpecific;
     rustc = rustSpecific;
   };
 in
   nightlyRustPlatform.buildRustPackage rec {
-    pname = "linkleaner";
-    version = "1.5.0";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "msfjarvis";
-      repo = pname;
-      rev = "v${version}";
-      sha256 = "sha256-Yhyi7XsC/j2yPxXcEF/UROVzsjeJ5mRk+K4evRrlTfI=";
-    };
+    inherit pname version src;
 
     buildInputs =
       pkgs.lib.optionals stdenv.isDarwin
