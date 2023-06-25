ac023cea1d5861e4f48eab3f0809373958624e0a
Harsh Shandilya <me@msfjarvis.dev>
2023-06-26 00:58:37 +0530

    feat(flakes): adopt nix-systems for flake systems

--- flake.nix
+++ flake.nix
@@ -1,31 +1,31 @@
 {
   description = "My personal NUR repository";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    rust-manifest = {
-      url = "https://static.rust-lang.org/dist/2023-06-10/channel-rust-nightly.toml";
-      flake = false;
-    };
-  };
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  inputs.systems.url = "github:msfjarvis/flake-systems";
+
+  inputs.fenix.url = "github:nix-community/fenix";
+  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.rust-manifest.url = "https://static.rust-lang.org/dist/2023-06-10/channel-rust-nightly.toml";
+  inputs.rust-manifest.flake = false;
+
   outputs = {
     self,
     nixpkgs,
     fenix,
+    systems,
     rust-manifest,
   }: let
-    systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-    pkgs = forAllSystems (system: (nixpkgs.legacyPackages.${system}.extend fenix.overlays.default));
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+    pkgs = eachSystem (system: (nixpkgs.legacyPackages.${system}.extend fenix.overlays.default));
     packagesFn = pkgs:
       import ./default.nix {
         inherit pkgs rust-manifest;
         fenix = import fenix {inherit pkgs;};
       };
   in {
-    packages = forAllSystems (system: packagesFn pkgs.${system});
+    packages = eachSystem (system: packagesFn pkgs.${system});
   };
 }
